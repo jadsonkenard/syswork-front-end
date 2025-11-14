ff03d136325f615ad8b467c3dd784981f2506a28
Jadson <jadsonkenard@gmail.com>
2025-11-14 13:55:54 -0300

    Feat: Cria o Header

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import { Layout } from "./../components";
 import { Home, Notfound } from "../pages";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-  },
-  {
-    path: "*",
-    element: <Notfound />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "*",
+        element: <Notfound />,
+      },
+    ],
   },
 ]);
