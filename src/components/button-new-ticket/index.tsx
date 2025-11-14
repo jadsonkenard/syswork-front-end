57dbae867d5a98bde1c8c71ce03b638d5bc67c42
Jadson <jadsonkenard@gmail.com>
2025-11-14 18:00:57 -0300

    Feat: Adiciona activity indicator no button

--- src/components/button-new-ticket/index.tsx
+++ src/components/button-new-ticket/index.tsx
@@ -1,19 +1,22 @@
 import styles from "./New-ticket.module.css";
+import { Dots } from "react-activity";
 
 type ButtonNewTicketProps = {
   title: string;
   disabled?: boolean;
+  isLoading: boolean;
   onClick: () => void;
 };
 
 export default function ButtonNewTicket({
   title,
   disabled = false,
+  isLoading = false,
   onClick,
 }: ButtonNewTicketProps) {
   return (
     <button className={styles.container} disabled={disabled} onClick={onClick}>
-      {title}
+      {isLoading ? <Dots color="#FFF" size={16} /> : title}
     </button>
   );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { ButtonNewTicket } from "../../components";
+
 export default function Home() {
+  const [loading, setLoading] = useState(false);
   return (
     <div>
       <h1>Home</h1>
       <ButtonNewTicket
         title="Novo chamado"
-        onClick={() => console.log("Ativo")}
+        onClick={() => setLoading(!loading)}
+        isLoading={loading}
       />
     </div>
   );
