c4e47d5462955dcdd287f17f6dfe7c6d2f38eaa5
Jadson <jadsonkenard@gmail.com>
2025-11-14 16:39:17 -0300

    Refactor: Refatora posicionamento do sidebar em relação ao header

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../sidebar";
+import Header from "../header";
 import styles from "./Layout.module.css";
 
 export default function Layout() {
   return (
-    <div className={styles.container}>
+    <div className={styles.layout}>
       <Sidebar />
-      <main className={styles.content}>
-        <Outlet />
-      </main>
+      <div className={styles.rightArea}>
+        <Header />
+        <main className={styles.content}>
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
