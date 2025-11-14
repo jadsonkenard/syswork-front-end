d250094bfbc75827920b9a48007e3513186be7af
Jadson <jadsonkenard@gmail.com>
2025-11-14 18:12:48 -0300

    Feat: Instala biblioteca de ícones lucide-react

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
+import ButtonNewTicket from "../button-new-ticket";
+import { Clipboard } from "lucide-react";
 import styles from "./Sidebar.module.css";
 
 export default function Sidebar() {
+  const [loading, setLoading] = useState(false);
   return (
     <div>
       <aside className={styles.sidebar}>
         <div className={styles.logoArea}>LOGO</div>
         <div className={styles["box-new-ticket"]}>
-          <button>Novo chamado</button>
+          <ButtonNewTicket
+            title="Novo chamado"
+            onClick={() => setLoading(!loading)}
+            isLoading={loading}
+          />
         </div>
+        <Clipboard size={20} />
         <p>Opção 1</p>
         <p>Opção 1</p>
         <p>Opção 1</p>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
-import { ButtonNewTicket } from "../../components";
-
 export default function Home() {
-  const [loading, setLoading] = useState(false);
   return (
     <div>
       <h1>Home</h1>
-      <ButtonNewTicket
-        title="Novo chamado"
-        onClick={() => setLoading(!loading)}
-        isLoading={loading}
-      />
     </div>
   );
 }
