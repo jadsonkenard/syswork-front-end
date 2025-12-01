8840ed40fd898b1cb294c614cce8e5d61e6ccb5e
Jadson <jadsonkenard@gmail.com>
2025-12-01 15:22:29 -0300

    Feat: Estiliza página de notfound

--- src/pages/not-found/index.tsx
+++ src/pages/not-found/index.tsx
@@ -1,6 +1,8 @@
+import styles from "./Not-found.module.css"
+
 export default function Notfound() {
   return (
-    <div>
+    <div className={styles.container}>
       <h3>syswork diz:</h3>
       <h1>Error. 404 NotFound. Página não encontrada.</h1>
     </div>
