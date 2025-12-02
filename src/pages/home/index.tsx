e57ffe7d64a9ce5270c90d5b7b4ef2e07456894f
Jadson <jadsonkenard@gmail.com>
2025-12-02 17:27:06 -0300

    Feat: Cria método para passar o ID do chamado da home para detail

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,12 +1,14 @@
 import styles from "./Home.module.css";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  const navigate = useNavigate();
+  const ticketExemple = 77;
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Home</h2>
-      <button onClick={() => navigate("/ticketdetail")}>IR</button>
+      <Link to="/ticketdetail" state={{ ticketExemple }}>
+        Ver detalhes
+      </Link>
     </div>
   );
 }
--- src/pages/ticket-detail/index.tsx
+++ src/pages/ticket-detail/index.tsx
@@ -1,6 +1,13 @@
+import { useLocation } from "react-router-dom";
 import styles from "./TicketDetail.module.css";
 
 export default function TicketDetail() {
+  const { state } = useLocation();
+
+  const ticketExemple = state?.ticketExemple;
+
+  console.log(ticketExemple);
+
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>Detalhes</h2>
@@ -8,10 +15,11 @@ export default function TicketDetail() {
             1- AO CLICAR EM ASSUMIR O STATUS DEVE SER ALTERADO PARA "IN PROGRESS"
             2- BOTÃO DE ASSUMIR DEVE TER UMA RENDERIZAÇÃO CONDICIONAL ONDE, SE O STATUS DO CHAMADO
             FOR IN PROGRESS, O TEXTE DEVER MUDAR PARA CONCLUIR.
+            3- ID ID VINDO DA HOME DEVE SER BUSCADO NA API. NOVA ROTA??????
       */}
 
       <button>Assumir</button>
-      <p>id: {}</p>
+      <p>id: {ticketExemple}</p>
       <p>Título: {}</p>
       <p>Descrição: {}</p>
       <p>Status: {}</p>
